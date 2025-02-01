e26d31fdefe323b2a257bdc84712d6f1357e568b
nakjun <111031253+nakjun12@users.noreply.github.com>
2025-02-01 09:12:55 +0900

    add number-of-1-bits

---
+++ number-of-1-bits/nakjun12.ts
@@ -0,0 +1,10 @@
+/*
+ * TC: O(logn)
+ * SC: O(logn)
+ * */
+function hammingWeight(n: number): number {
+  const MAX_NUM = 2147483648 - 1;
+
+  const bitwiseOperated = (n & MAX_NUM).toString(2);
+  return bitwiseOperated.replaceAll("0", "").length;
+}
